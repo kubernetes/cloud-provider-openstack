a70b3ebcd72f159079cbaacf5c343d694f8807ac
Davanum Srinivas <davanum@gmail.com>
2017-04-24 14:47:54 -0400

    Generate junit report
    Change-Id: I583e9492ebe5a9e94aac87ab48c4cb3c4c37a2d8

--- contrib/post_test_hook.sh
+++ contrib/post_test_hook.sh
@@ -144,5 +144,5 @@ sudo -E PATH=$GOPATH/bin:$PATH make all WHAT=vendor/github.com/onsi/ginkgo/ginkg
 source $DEST/.gimme/envs/go1.7.5.env
 
 sudo -E PATH=$GOPATH/bin:$PATH make all WHAT=test/e2e/e2e.test
-sudo -E PATH=$GOPATH/bin:$PATH go run hack/e2e.go -- -v --test --test_args="--ginkgo.trace=true --ginkgo.seed=1378936983 --ginkgo.v --ginkgo.skip=$(test_names)"
+sudo -E PATH=$GOPATH/bin:$PATH go run hack/e2e.go -- -v --test --test_args="--ginkgo.trace=true --ginkgo.seed=1378936983 --logtostderr --v 4 --report-dir=/opt/stack/logs/ --ginkgo.v --ginkgo.skip=$(test_names)"
 popd >/dev/null
