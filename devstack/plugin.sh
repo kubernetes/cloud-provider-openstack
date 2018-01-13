openstack-archive/k8s-cloud-provider

Use older docker version w/ k8s, switch off CRI
Change-Id: If324073a8ecf7f6fbce44e65a5a3f3267355bcff

devstack/plugin.sh
@@ -33,7 +33,12 @@ function install_prereqs {
 function install_docker {
     # Install docker if needed
     if ! is_package_installed docker-engine; then
-        curl -sSL https://get.docker.io | sudo bash
+        sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+        sudo apt-add-repository 'deb https://apt.dockerproject.org/repo ubuntu-xenial main'
+        sudo apt-get update
+        sudo apt-cache policy docker-engine
+        sudo apt-get install -y docker-engine=1.12.6-0~ubuntu-xenial
+        sudo systemctl status docker
     fi
     docker --version
 
@@ -90,7 +95,7 @@ function install_k8s_cloud_provider {
     sudo touch $LOG_DIR/kube-scheduler.log;sudo ln -s $LOG_DIR/kube-scheduler.log $LOG_DIR/screen-kube-scheduler.log
     sudo touch $LOG_DIR/kubelet.log;sudo ln -s $LOG_DIR/kubelet.log $LOG_DIR/screen-kubelet.log
 
-    # Turn on a few things in local-up-cluster.sh
+    # Turn on/off a few things in local-up-cluster.sh
     export ALLOW_PRIVILEGED=true
     export ALLOW_SECURITY_CONTEXT=true
     export ALLOW_ANY_TOKEN=true
@@ -99,6 +104,8 @@ function install_k8s_cloud_provider {
     export FIRST_SERVICE_CLUSTER_IP=10.1.0.1
     export API_HOST_IP=${HOST_IP:-"127.0.0.1"}
     export KUBELET_HOST=${HOST_IP:-"127.0.0.1"}
+    #export HOSTNAME_OVERRIDE=${HOST_IP:-"127.0.0.1"}
+    export ENABLE_CRI=false
 
     run_process kubernetes "sudo -E PATH=$PATH hack/local-up-cluster.sh"
     popd >/dev/null

kubernetes/cloud-provider-openstack

devstack/plugin.sh
@@ -33,7 +33,12 @@ function install_prereqs {
 function install_docker {
     # Install docker if needed
     if ! is_package_installed docker-engine; then
-        curl -sSL https://get.docker.io | sudo bash
+        sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+        sudo apt-add-repository 'deb http://apt.dockerproject.org/repo ubuntu-xenial main'
+        sudo apt-get update
+        sudo apt-cache policy docker-engine
+        sudo apt-get install -y docker-engine=1.12.6-0~ubuntu-xenial
+        sudo systemctl status docker
     fi
     docker --version
 
@@ -90,7 +95,7 @@ function install_k8s_cloud_provider {
     sudo touch $LOG_DIR/kube-scheduler.log;sudo ln -s $LOG_DIR/kube-scheduler.log $LOG_DIR/screen-kube-scheduler.log
     sudo touch $LOG_DIR/kubelet.log;sudo ln -s $LOG_DIR/kubelet.log $LOG_DIR/screen-kubelet.log
 
-    # Turn on a few things in local-up-cluster.sh
+    # Turn on/off a few things in local-up-cluster.sh
     export ALLOW_PRIVILEGED=true
     export ALLOW_SECURITY_CONTEXT=true
     export ALLOW_ANY_TOKEN=true
@@ -99,6 +104,8 @@ function install_k8s_cloud_provider {
     export FIRST_SERVICE_CLUSTER_IP=10.1.0.1
     export API_HOST_IP=${HOST_IP:-"127.0.0.1"}
     export KUBELET_HOST=${HOST_IP:-"127.0.0.1"}
+    #export HOSTNAME_OVERRIDE=${HOST_IP:-"127.0.0.1"}
+    export ENABLE_CRI=false
 
     run_process kubernetes "sudo -E PATH=$PATH hack/local-up-cluster.sh"
     popd >/dev/null
@@ -126,8 +133,8 @@ if is_service_enabled k8s-cloud-provider; then
         :
 
     elif [[ "$1" == "stack" && "$2" == "install"  ]]; then
-        install_prereqs
         install_docker
+        install_prereqs
 
     elif [[ "$1" == "stack" && "$2" == "post-config"  ]]; then
         install_k8s_cloud_provider