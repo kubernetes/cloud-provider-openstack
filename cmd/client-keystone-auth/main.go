42c258b26225f10cdb019876d8fadafabd2c859e
Mike Fedosin <mfedosin@redhat.com>
2018-08-13 17:51:25 +0200

    Start using client.authentication.k8s.io/v1beta1 api for plugins
    Recently client authentication api was updated from v1alpha1 to v1beta1.
    
    This commit switches keystone-client-auth on the new api and removes the outdated
    one.
    
    The difference between two versions is that in alpha interactive mode was checked
    in client-go and provided to the plugin with the environment variable
    KUBERNETES_EXEC_INFO. In beta the plugin uses a TTY check to determine if it's
    appropriate to prompt a user interactively. That's why KUBERNETES_EXEC_INFO is
    not used anymore and can be removed.
    
    Another change is that ExecCredentialSpec struct doesn't contain any fields now,
    which means the plugin shouldn't write anything in this section during printing
    response output.

--- cmd/client-keystone-auth/main.go
+++ cmd/client-keystone-auth/main.go
@@ -17,7 +17,6 @@ limitations under the License.
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"io"
 	"os"
@@ -30,23 +29,17 @@ import (
 	"golang.org/x/crypto/ssh/terminal"
 
 	kflag "k8s.io/apiserver/pkg/util/flag"
-	"k8s.io/client-go/pkg/apis/clientauthentication/v1alpha1"
 	"k8s.io/cloud-provider-openstack/pkg/identity/keystone"
 )
 
 const errRespTemplate string = `{
-	"apiVersion": "client.authentication.k8s.io/v1alpha1",
+	"apiVersion": "client.authentication.k8s.io/v1beta1",
 	"kind": "ExecCredential",
-	"spec": {
-		"response": {
-			"code": 401,
-			"header": {},
-		},
-	}
+	"status": {}
 }`
 
 const respTemplate string = `{
-	"apiVersion": "client.authentication.k8s.io/v1alpha1",
+	"apiVersion": "client.authentication.k8s.io/v1beta1",
 	"kind": "ExecCredential",
 	"status": {
 		"token": "%v",
@@ -62,13 +55,9 @@ func promptForString(field string, r io.Reader, show bool) (result string, err e
 		_, err = fmt.Fscan(r, &result)
 	} else {
 		var data []byte
-		if terminal.IsTerminal(int(os.Stdin.Fd())) {
-			data, err = terminal.ReadPassword(int(os.Stdin.Fd()))
-			result = string(data)
-			fmt.Fprintf(os.Stderr, "\n")
-		} else {
-			return "", fmt.Errorf("error reading input for %s", field)
-		}
+		data, err = terminal.ReadPassword(int(os.Stdin.Fd()))
+		result = string(data)
+		fmt.Fprintf(os.Stderr, "\n")
 	}
 	return result, err
 }
@@ -125,27 +114,6 @@ func prompt(url string, domain string, user string, project string, password str
 	return options, nil
 }
 
-// KuberneteExecInfo holds information passed to the plugin by the transport. This
-// contains runtime specific information, such as if the session is interactive,
-// auth API version and kind of request.
-type KuberneteExecInfo struct {
-	APIVersion string                      `json:"apiVersion"`
-	Kind       string                      `json:"kind"`
-	Spec       v1alpha1.ExecCredentialSpec `json:"spec"`
-}
-
-func validateKebernetesExecInfo(kei KuberneteExecInfo) error {
-	if kei.APIVersion != v1alpha1.SchemeGroupVersion.String() {
-		return fmt.Errorf("unsupported API version: %v", kei.APIVersion)
-	}
-
-	if kei.Kind != "ExecCredential" {
-		return fmt.Errorf("incorrect request kind: %v", kei.Kind)
-	}
-
-	return nil
-}
-
 func main() {
 	var url string
 	var domain string
@@ -162,28 +130,9 @@ func main() {
 	pflag.StringVar(&password, "password", os.Getenv("OS_PASSWORD"), "Password")
 	kflag.InitFlags()
 
-	keiData := os.Getenv("KUBERNETES_EXEC_INFO")
-	if keiData == "" {
-		fmt.Fprintln(os.Stderr, "KUBERNETES_EXEC_INFO env variable must be set.")
-		os.Exit(1)
-	}
-
-	kei := KuberneteExecInfo{}
-	err = json.Unmarshal([]byte(keiData), &kei)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "Failed to parse KUBERNETES_EXEC_INFO value.")
-		os.Exit(1)
-	}
-
-	err = validateKebernetesExecInfo(kei)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "An error occurred: %v\n", err)
-		os.Exit(1)
-	}
-
 	// Generate Gophercloud Auth Options based on input data from stdin
-	// if "intaractive" is set to true, or from env variables otherwise.
-	if !kei.Spec.Interactive {
+	// if IsTerminal returns "true", or from env variables otherwise.
+	if !terminal.IsTerminal(int(os.Stdin.Fd())) {
 		options, err = openstack.AuthOptionsFromEnv()
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "Failed to read openstack env vars: %s", err)
@@ -201,6 +150,7 @@ func main() {
 	if err != nil {
 		if _, ok := err.(gophercloud.ErrDefault401); ok {
 			fmt.Println(errRespTemplate)
+			os.Stderr.WriteString("Invalid user credentials were provided\n")
 			os.Exit(0)
 		}
 		fmt.Fprintf(os.Stderr, "An error occurred: %v\n", err)
