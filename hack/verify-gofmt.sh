80f757d4c2256bb03eeae8075040fce5d770e8a7
jichenjc <jichenjc@cn.ibm.com>
2018-11-16 13:21:58 +0800

    update format check scripts
    per
    https://github.com/kubernetes/kubernetes/blob/843a67b215dcfa1781820a797c4c759e42e81d3a/hack/verify-gofmt.sh#L52-L56

--- hack/verify-gofmt.sh
+++ hack/verify-gofmt.sh
@@ -22,7 +22,11 @@ if ! which gofmt > /dev/null; then
   exit 1
 fi
 
-diff=$(find . -name "*.go" | grep -v "\/vendor\/" | xargs gofmt -s -d 2>&1)
+# gofmt exits with non-zero exit code if it finds a problem unrelated to
+# formatting (e.g., a file does not parse correctly). Without "|| true" this
+# would have led to no useful error message from gofmt, because the script would
+# have failed before getting to the "echo" in the block below.
+diff=$(find . -name "*.go" | grep -v "\/vendor\/" | xargs gofmt -s -d 2>&1) || true
 if [[ -n "${diff}" ]]; then
   echo "${diff}"
   echo
