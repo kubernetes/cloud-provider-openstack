7f580ddb53b9e403585f6dfeb1faeb56fecc825c
ji chen <jichenjc@cn.ibm.com>
2019-03-13 13:09:33 +0800

    Add probe for CSI (#508)
    Add CSI identity CSI so that we can return CSI status back
    
    fixes: 483

--- pkg/csi/cinder/identityserver.go
+++ pkg/csi/cinder/identityserver.go
@@ -21,6 +21,7 @@ import (
 	"golang.org/x/net/context"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
+	"k8s.io/cloud-provider-openstack/pkg/csi/cinder/openstack"
 	"k8s.io/klog"
 )
 
@@ -46,6 +47,11 @@ func (ids *identityServer) GetPluginInfo(ctx context.Context, req *csi.GetPlugin
 }
 
 func (ids *identityServer) Probe(ctx context.Context, req *csi.ProbeRequest) (*csi.ProbeResponse, error) {
+	_, err := openstack.CreateOpenStackProvider()
+	if err != nil {
+		klog.V(3).Infof("Failed to CreateOpenStackProvider: %v", err)
+		return nil, status.Error(codes.FailedPrecondition, "Failed to communicate with openstack")
+	}
 	return &csi.ProbeResponse{}, nil
 }
 
