c4994387010b7e7034fb2c70b29e8fc47f962722
kayrus <kayrus@users.noreply.github.com>
2019-10-10 07:30:53 +0200

    Barbican KMS: use persistent client (#788)

--- pkg/kms/barbican/fake_barbican.go
+++ pkg/kms/barbican/fake_barbican.go
@@ -5,7 +5,6 @@ import "encoding/hex"
 type FakeBarbican struct {
 }
 
-func (client *FakeBarbican) GetSecret(cfg Config) ([]byte, error) {
+func (client *FakeBarbican) GetSecret(keyID string) ([]byte, error) {
 	return hex.DecodeString("6368616e676520746869732070617373")
-
 }
