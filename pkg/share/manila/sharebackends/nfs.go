1fa8b21d3372b6ce7e8c48f0d5c3ac5f4c71c172
Róbert Vašek <robert.vasek@cern.ch>
2020-01-15 06:53:32 +0100

    [manila-provisioner] client code refactoring (#838)
    * removed hardcoded user agent name from manilaclient
    
    * manila-provisioner refactoring: use pkg/csi/manila/manilaclient as manila client
    
    * fixed tests

--- pkg/share/manila/sharebackends/nfs.go
+++ pkg/share/manila/sharebackends/nfs.go
@@ -47,11 +47,11 @@ func (NFS) BuildSource(args *BuildSourceArgs) (*v1.PersistentVolumeSource, error
 
 // GrantAccess to NFS share. Allows read-write access to everyone by default!
 func (NFS) GrantAccess(args *GrantAccessArgs) (*shares.AccessRight, error) {
-	return shares.GrantAccess(args.Client, args.Share.ID, shares.GrantAccessOpts{
+	return args.Client.GrantAccess(args.Share.ID, shares.GrantAccessOpts{
 		AccessType:  "ip",
 		AccessTo:    args.Options.NFSShareClient,
 		AccessLevel: "rw",
-	}).Extract()
+	})
 }
 
 // RevokeAccess does nothing - nothing to release
