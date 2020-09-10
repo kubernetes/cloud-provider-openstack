5a626f67b61f56f5c6a55269b76443f26f9eadef
Sean Schneeweiss <6184685+seanschneeweiss@users.noreply.github.com>
2020-09-10 04:35:44 +0200

    [all] Update k8s dependencies to v1.19.0 (#1151)
    * [occm] update cloud-provider to 1.19.0
    
    This commit updates k8s version to 1.19.0
    to use the cloud-provider pkg from its new location.
    
    Signed-off-by: Sean Schneeweiss <sean.schneeweiss@daimler.com>
    
    * [occm] use version v1.19.0, do not use InstancesV2
    
    InstancesV2 requires further testing before being used.
    
    Signed-off-by: Sean Schneeweiss <sean.schneeweiss@daimler.com>

--- pkg/cloudprovider/providers/openstack/openstack_instances.go
+++ pkg/cloudprovider/providers/openstack/openstack_instances.go
@@ -46,6 +46,16 @@ const (
 
 // Instances returns an implementation of Instances for OpenStack.
 func (os *OpenStack) Instances() (cloudprovider.Instances, bool) {
+	return os.instances()
+}
+
+// InstancesV2 returns an implementation of InstancesV2 for OpenStack.
+// TODO: Support InstancesV2 in the future.
+func (os *OpenStack) InstancesV2() (cloudprovider.InstancesV2, bool) {
+	return nil, false
+}
+
+func (os *OpenStack) instances() (*Instances, bool) {
 	klog.V(4).Info("openstack.Instances() called")
 
 	compute, err := os.NewComputeV2()
@@ -120,7 +130,12 @@ func (i *Instances) NodeAddressesByProviderID(ctx context.Context, providerID st
 	return addresses, nil
 }
 
-// InstanceExistsByProviderID returns true if the instance with the given provider id still exist.
+// InstanceExists returns true if the instance for the given node exists.
+func (i *Instances) InstanceExists(ctx context.Context, node *v1.Node) (bool, error) {
+	return i.InstanceExistsByProviderID(ctx, node.Spec.ProviderID)
+}
+
+// InstanceExistsByProviderID returns true if the instance with the given provider id still exists.
 // If false is returned with no error, the instance will be immediately deleted by the cloud controller manager.
 func (i *Instances) InstanceExistsByProviderID(ctx context.Context, providerID string) (bool, error) {
 	instanceID, err := instanceIDFromProviderID(providerID)
@@ -139,7 +154,14 @@ func (i *Instances) InstanceExistsByProviderID(ctx context.Context, providerID s
 	return true, nil
 }
 
-// InstanceShutdownByProviderID returns true if the instances is in safe state to detach volumes
+// InstanceShutdown returns true if the instances is in safe state to detach volumes.
+// It is the only state, where volumes can be detached immediately.
+func (i *Instances) InstanceShutdown(ctx context.Context, node *v1.Node) (bool, error) {
+	return i.InstanceShutdownByProviderID(ctx, node.Spec.ProviderID)
+}
+
+// InstanceShutdownByProviderID returns true if the instances is in safe state to detach volumes.
+// It is the only state, where volumes can be detached immediately.
 func (i *Instances) InstanceShutdownByProviderID(ctx context.Context, providerID string) (bool, error) {
 	instanceID, err := instanceIDFromProviderID(providerID)
 	if err != nil {
@@ -156,6 +178,39 @@ func (i *Instances) InstanceShutdownByProviderID(ctx context.Context, providerID
 		return true, nil
 	}
 	return false, nil
+}
+
+// InstanceMetadata returns metadata of the specified instance.
+func (i *Instances) InstanceMetadata(ctx context.Context, node *v1.Node) (*cloudprovider.InstanceMetadata, error) {
+	instanceID, err := instanceIDFromProviderID(node.Spec.ProviderID)
+	if err != nil {
+		return nil, err
+	}
+
+	srv, err := servers.Get(i.compute, instanceID).Extract()
+	if err != nil {
+		return nil, err
+	}
+
+	instanceType, err := srvInstanceType(srv)
+	if err != nil {
+		return nil, err
+	}
+
+	interfaces, err := getAttachedInterfacesByID(i.compute, srv.ID)
+	if err != nil {
+		return nil, err
+	}
+	addresses, err := nodeAddresses(srv, interfaces, i.networkingOpts)
+	if err != nil {
+		return nil, err
+	}
+
+	return &cloudprovider.InstanceMetadata{
+		ProviderID:    node.Spec.ProviderID,
+		InstanceType:  instanceType,
+		NodeAddresses: addresses,
+	}, nil
 }
 
 // InstanceID returns the kubelet's cloud provider ID.
