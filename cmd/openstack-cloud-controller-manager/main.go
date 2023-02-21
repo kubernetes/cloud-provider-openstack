680f9cfe2a87512f3fa54d8a374c0771833de370
Pierre Prinetti <pierreprinetti@redhat.com>
2023-02-21 10:37:49 +0100

    [release-1.25][occm] Reintroduce seeding of math/rand source (#2123)
    * [occm] Revert seeding of math/rand source
    
    In Go v1.20, `rand.Seed` is deprecated and `math/rand`'s is
    automatically seeded. However, that doesn't happen in Go v1.19 that is
    used to build this branch.
    
    With this commit, the global source for `math/rand` is seeded again in
    `main.go`.
    
    * go-lint: Upgrade staticcheck to v0.4.2
    
    which handles deprecations more gracefully.

--- cmd/openstack-cloud-controller-manager/main.go
+++ cmd/openstack-cloud-controller-manager/main.go
@@ -44,7 +44,7 @@ import (
 )
 
 func main() {
-	rand.NewSource(time.Now().UnixNano())
+	rand.Seed(time.Now().UnixNano())
 
 	ccmOptions, err := options.NewCloudControllerManagerOptions()
 	if err != nil {
