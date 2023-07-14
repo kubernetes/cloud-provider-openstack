318b4c135d3a5addf3381a6106ea2c13da2f1a93
Michal Dulko <mdulko@redhat.com>
2023-07-14 04:45:29 +0200

    [occm] Delete unused SG rules with manage-security-groups (#2287)
    * Apply LB SG to nodes only once
    
    Code applying SG to nodes was incorrectly put in a loop over Services
    ports. This commit fixes that.
    
    * Delete unused SG rules with manage-security-groups
    
    This commit makes sure unnecessary SG rules are removed when we're
    reconciling a Service.
    
    Co-Authored-By: Sven Haardiek <sven.haardiek@uni-muenster.de>
    
    ---------
    
    Co-authored-by: Sven Haardiek <sven.haardiek@uni-muenster.de>

--- pkg/openstack/loadbalancer_test.go
+++ pkg/openstack/loadbalancer_test.go
@@ -7,6 +7,7 @@ import (
 	"github.com/stretchr/testify/assert"
 
 	"github.com/gophercloud/gophercloud/openstack/loadbalancer/v2/listeners"
+	"github.com/gophercloud/gophercloud/openstack/networking/v2/extensions/security/rules"
 )
 
 type testPopListener struct {
@@ -131,5 +132,303 @@ func TestPopListener(t *testing.T) {
 		sort.Strings(item.result)
 		sort.Strings(ids)
 		assert.Equal(t, ids, item.result, item.name)
+	}
+}
+
+type testGetRulesToCreateAndDelete struct {
+	testName      string
+	wantedRules   []rules.CreateOpts
+	existingRules []rules.SecGroupRule
+	toCreate      []rules.CreateOpts
+	toDelete      []rules.SecGroupRule
+}
+
+func TestGetRulesToCreateAndDelete(t *testing.T) {
+	tests := []testGetRulesToCreateAndDelete{
+		{
+			testName:      "Empty elements",
+			wantedRules:   []rules.CreateOpts{},
+			existingRules: []rules.SecGroupRule{},
+			toCreate:      []rules.CreateOpts{},
+			toDelete:      []rules.SecGroupRule{},
+		},
+		{
+			testName: "Removal of default egress SG rules",
+			wantedRules: []rules.CreateOpts{
+				{
+					Direction:      "ingress",
+					EtherType:      "IPv4",
+					SecGroupID:     "foo",
+					PortRangeMax:   123,
+					PortRangeMin:   123,
+					Protocol:       "TCP",
+					RemoteIPPrefix: "10.0.0.0/8",
+				},
+			},
+			existingRules: []rules.SecGroupRule{
+				{
+					ID:             "bar",
+					Direction:      "egress",
+					EtherType:      "IPv4",
+					SecGroupID:     "foo",
+					RemoteIPPrefix: "0.0.0.0/0",
+				}, {
+					ID:             "baz",
+					Direction:      "egress",
+					EtherType:      "IPv6",
+					SecGroupID:     "foo",
+					RemoteIPPrefix: "::/0",
+				},
+			},
+			toCreate: []rules.CreateOpts{
+				{
+					Direction:      "ingress",
+					EtherType:      "IPv4",
+					SecGroupID:     "foo",
+					PortRangeMax:   123,
+					PortRangeMin:   123,
+					Protocol:       "TCP",
+					RemoteIPPrefix: "10.0.0.0/8",
+				},
+			},
+			toDelete: []rules.SecGroupRule{
+				{
+					ID:             "bar",
+					Direction:      "egress",
+					EtherType:      "IPv4",
+					SecGroupID:     "foo",
+					RemoteIPPrefix: "0.0.0.0/0",
+				}, {
+					ID:             "baz",
+					Direction:      "egress",
+					EtherType:      "IPv6",
+					SecGroupID:     "foo",
+					RemoteIPPrefix: "::/0",
+				},
+			},
+		},
+		{
+			testName: "changing a port number",
+			wantedRules: []rules.CreateOpts{
+				{
+					Direction:      "ingress",
+					EtherType:      "IPv4",
+					SecGroupID:     "foo",
+					PortRangeMax:   124,
+					PortRangeMin:   124,
+					Protocol:       "TCP",
+					RemoteIPPrefix: "10.0.0.0/8",
+				},
+			},
+			existingRules: []rules.SecGroupRule{
+				{
+					ID:             "bar",
+					Direction:      "ingress",
+					EtherType:      "IPv4",
+					SecGroupID:     "foo",
+					RemoteIPPrefix: "10.0.0.0/8",
+					PortRangeMax:   123,
+					PortRangeMin:   123,
+				},
+			},
+			toCreate: []rules.CreateOpts{
+				{
+					Direction:      "ingress",
+					EtherType:      "IPv4",
+					SecGroupID:     "foo",
+					PortRangeMax:   124,
+					PortRangeMin:   124,
+					Protocol:       "TCP",
+					RemoteIPPrefix: "10.0.0.0/8",
+				},
+			},
+			toDelete: []rules.SecGroupRule{
+				{
+					ID:             "bar",
+					Direction:      "ingress",
+					EtherType:      "IPv4",
+					SecGroupID:     "foo",
+					RemoteIPPrefix: "10.0.0.0/8",
+					PortRangeMax:   123,
+					PortRangeMin:   123,
+				},
+			},
+		},
+		{
+			testName: "changing the CIDR",
+			wantedRules: []rules.CreateOpts{
+				{
+					Direction:      "ingress",
+					EtherType:      "IPv4",
+					SecGroupID:     "foo",
+					PortRangeMax:   123,
+					PortRangeMin:   123,
+					Protocol:       "TCP",
+					RemoteIPPrefix: "10.0.0.0/24",
+				},
+			},
+			existingRules: []rules.SecGroupRule{
+				{
+					ID:             "bar",
+					Direction:      "ingress",
+					EtherType:      "IPv4",
+					SecGroupID:     "foo",
+					RemoteIPPrefix: "10.0.0.0/8",
+					PortRangeMax:   123,
+					PortRangeMin:   123,
+				},
+			},
+			toCreate: []rules.CreateOpts{
+				{
+					Direction:      "ingress",
+					EtherType:      "IPv4",
+					SecGroupID:     "foo",
+					PortRangeMax:   123,
+					PortRangeMin:   123,
+					Protocol:       "TCP",
+					RemoteIPPrefix: "10.0.0.0/24",
+				},
+			},
+			toDelete: []rules.SecGroupRule{
+				{
+					ID:             "bar",
+					Direction:      "ingress",
+					EtherType:      "IPv4",
+					SecGroupID:     "foo",
+					RemoteIPPrefix: "10.0.0.0/8",
+					PortRangeMax:   123,
+					PortRangeMin:   123,
+				},
+			},
+		},
+		{
+			testName:    "wiping all rules",
+			wantedRules: []rules.CreateOpts{},
+			existingRules: []rules.SecGroupRule{
+				{
+					ID:             "bar",
+					Direction:      "ingress",
+					EtherType:      "IPv4",
+					SecGroupID:     "foo",
+					RemoteIPPrefix: "10.0.0.0/8",
+					PortRangeMax:   123,
+					PortRangeMin:   123,
+				},
+				{
+					ID:             "bar",
+					Direction:      "ingress",
+					EtherType:      "IPv4",
+					SecGroupID:     "foo",
+					RemoteIPPrefix: "10.0.0.0/8",
+					PortRangeMax:   124,
+					PortRangeMin:   124,
+				},
+				{
+					ID:             "bar",
+					Direction:      "ingress",
+					EtherType:      "IPv4",
+					SecGroupID:     "foo",
+					RemoteIPPrefix: "10.0.0.0/8",
+					PortRangeMax:   125,
+					PortRangeMin:   125,
+				},
+			},
+			toCreate: []rules.CreateOpts{},
+			toDelete: []rules.SecGroupRule{
+				{
+					ID:             "bar",
+					Direction:      "ingress",
+					EtherType:      "IPv4",
+					SecGroupID:     "foo",
+					RemoteIPPrefix: "10.0.0.0/8",
+					PortRangeMax:   123,
+					PortRangeMin:   123,
+				},
+				{
+					ID:             "bar",
+					Direction:      "ingress",
+					EtherType:      "IPv4",
+					SecGroupID:     "foo",
+					RemoteIPPrefix: "10.0.0.0/8",
+					PortRangeMax:   124,
+					PortRangeMin:   124,
+				},
+				{
+					ID:             "bar",
+					Direction:      "ingress",
+					EtherType:      "IPv4",
+					SecGroupID:     "foo",
+					RemoteIPPrefix: "10.0.0.0/8",
+					PortRangeMax:   125,
+					PortRangeMin:   125,
+				},
+			},
+		},
+		{
+			testName: "several rules for an empty SG",
+			wantedRules: []rules.CreateOpts{
+				{
+					Direction:      "ingress",
+					EtherType:      "IPv4",
+					SecGroupID:     "foo",
+					PortRangeMax:   123,
+					PortRangeMin:   123,
+					Protocol:       "TCP",
+					RemoteIPPrefix: "10.0.0.0/8",
+				}, {
+					Direction:      "ingress",
+					EtherType:      "IPv4",
+					SecGroupID:     "foo",
+					PortRangeMax:   124,
+					PortRangeMin:   124,
+					Protocol:       "TCP",
+					RemoteIPPrefix: "10.0.10.0/24",
+				}, {
+					Direction:      "ingress",
+					EtherType:      "IPv4",
+					SecGroupID:     "foo",
+					PortRangeMax:   124,
+					PortRangeMin:   124,
+					Protocol:       "UDP",
+					RemoteIPPrefix: "10.0.12.0/24",
+				},
+			},
+			existingRules: []rules.SecGroupRule{},
+			toCreate: []rules.CreateOpts{
+				{
+					Direction:      "ingress",
+					EtherType:      "IPv4",
+					SecGroupID:     "foo",
+					PortRangeMax:   123,
+					PortRangeMin:   123,
+					Protocol:       "TCP",
+					RemoteIPPrefix: "10.0.0.0/8",
+				}, {
+					Direction:      "ingress",
+					EtherType:      "IPv4",
+					SecGroupID:     "foo",
+					PortRangeMax:   124,
+					PortRangeMin:   124,
+					Protocol:       "TCP",
+					RemoteIPPrefix: "10.0.10.0/24",
+				}, {
+					Direction:      "ingress",
+					EtherType:      "IPv4",
+					SecGroupID:     "foo",
+					PortRangeMax:   124,
+					PortRangeMin:   124,
+					Protocol:       "UDP",
+					RemoteIPPrefix: "10.0.12.0/24",
+				},
+			},
+			toDelete: []rules.SecGroupRule{},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.testName, func(t *testing.T) {
+			toCreate, toDelete := getRulesToCreateAndDelete(tt.wantedRules, tt.existingRules)
+			assert.ElementsMatch(t, tt.toCreate, toCreate)
+			assert.ElementsMatch(t, tt.toDelete, toDelete)
+		})
 	}
 }
