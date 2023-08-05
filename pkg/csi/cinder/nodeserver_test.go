e831136c3cbfb8c87fe0c984c53740835dcdcddf
Tobias Wolf <wolf@b1-systems.de>
2023-08-05 03:44:39 +0200

    Add support to only run selected CSI services of the cinder CSI driver

--- pkg/csi/cinder/nodeserver_test.go
+++ pkg/csi/cinder/nodeserver_test.go
@@ -38,7 +38,7 @@ var omock *openstack.OpenStackMock
 func init() {
 	if fakeNs == nil {
 
-		d := NewDriver(FakeEndpoint, FakeCluster)
+		d := NewDriver(&DriverOpts{ Endpoint: FakeEndpoint, ClusterID: FakeCluster })
 
 		// mock MountMock
 		mmock = new(mount.MountMock)
@@ -142,7 +142,7 @@ func TestNodePublishVolumeEphermeral(t *testing.T) {
 	metadata.MetadataService = metamock
 	openstack.OsInstance = omock
 
-	d := NewDriver(FakeEndpoint, FakeCluster)
+	d := NewDriver(&DriverOpts{ Endpoint: FakeEndpoint, ClusterID: FakeCluster })
 	fakeNse := NewNodeServer(d, mount.MInstance, metadata.MetadataService, openstack.OsInstance)
 
 	// Init assert
@@ -293,7 +293,7 @@ func TestNodeUnpublishVolumeEphermeral(t *testing.T) {
 	omock.On("WaitDiskDetached", FakeNodeID, FakeVolID).Return(nil)
 	omock.On("DeleteVolume", FakeVolID).Return(nil)
 
-	d := NewDriver(FakeEndpoint, FakeCluster)
+	d := NewDriver(&DriverOpts{ Endpoint: FakeEndpoint, ClusterID: FakeCluster })
 	fakeNse := NewNodeServer(d, mount.MInstance, metadata.MetadataService, openstack.OsInstance)
 
 	// Init assert
