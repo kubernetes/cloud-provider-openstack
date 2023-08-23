61e721b28f1d1037e56cecdefbb21562bf8b7a53
Tobias Wolf <wolf@b1-systems.de>
2023-08-23 16:57:06 +0200

    Clean up source files to successfully complete linting

--- pkg/csi/cinder/driver_test.go
+++ pkg/csi/cinder/driver_test.go
@@ -29,10 +29,11 @@ var (
 
 func NewFakeDriver() *Driver {
 
-	driver := NewDriver(&DriverOpts{ Endpoint: FakeEndpoint, ClusterID: FakeCluster })
+	driver := NewDriver(&DriverOpts{Endpoint: FakeEndpoint, ClusterID: FakeCluster})
 
 	return driver
 }
+
 func TestValidateControllerServiceRequest(t *testing.T) {
 	d := NewFakeDriver()
 
