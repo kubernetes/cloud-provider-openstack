4609adcc8d3a68609df7622c27297cafb3e92b6b
pýrus <kayrus@users.noreply.github.com>
2025-03-27 17:00:06 +0100

    fix lbaas logs

--- pkg/openstack/loadbalancer.go
+++ pkg/openstack/loadbalancer.go
@@ -1515,9 +1515,9 @@ func (lbaas *LbaasV2) makeSvcConf(serviceName string, service *corev1.Service, s
 	svcConf.nodeSelectors = getKeyValueFromServiceAnnotation(service, ServiceAnnotationLoadBalancerNodeSelector, lbaas.opts.NodeSelector)
 	for key, value := range svcConf.nodeSelectors {
 		if value == "" {
-			klog.V(3).InfoS("Target node label %s key is set to LoadBalancer service %s", key, serviceName)
+			klog.V(3).Infof("Target node label %s key is set to LoadBalancer service %s", key, serviceName)
 		} else {
-			klog.V(3).InfoS("Target node label %s=%s is set to LoadBalancer service %s", key, value, serviceName)
+			klog.V(3).Infof("Target node label %s=%s is set to LoadBalancer service %s", key, value, serviceName)
 		}
 	}
 
